2fd090754691c0b1f58e40a52eb996dc4078c9a5
Axel <axeltib@kth.se>
2020-09-22 15:27:29 +0200

    Clean up work

--- exc2/live_plot.py
+++ exc2/live_plot.py
@@ -6,14 +6,14 @@ import time, math
 import numpy as np
 
 class Plotter:
-    def __init__(self, x_min=0, x_max=10, dx = 0.001,
+    def __init__(self, x_min=0, x_max=10, dx = 0.01,
                        y_min=0, y_max=10, rate = 0.01):
         self.rate = rate
         plt.xlim(x_min, x_max)
         plt.ylim(y_min)
         self.fig, self.ax = plt.subplots()
         self.line, = self.ax.plot([], [], lw=2)
-        self.data = ([], [])
+        self.data = ([], [])    #For exporting uses
 
     def plot_data(self):
         """Plotting data"""
@@ -25,25 +25,22 @@ class Plotter:
         data tracking. """
         self.data[0].append(data[0])
         self.data[1].append(data[1])
-        self.line.set_data(self.data)
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
         plt.plot(data[0], data[1])
-        plt.draw()
+
+        self.line.set_data(self.data)
+        #self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
         time.sleep(self.rate)
 
-    def animation(self, offset):
-        self.line.set_data(self.x_array, self.function(offset))
-        return self.line,
 
 def main():
     plt.ion()
 
     plot = Plotter(0, 50, rate = 0.1)
     plot.plot_data()
-    for i in range(20):
-        print(i, math.sin(i))
-        plot.update((i, math.sin(i)))
+    for i in range(200):
+        x = 0.1*i
+        plot.update((x, math.sin(x)))
 
 
 if __name__ == '__main__':
