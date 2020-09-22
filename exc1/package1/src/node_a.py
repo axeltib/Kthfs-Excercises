dc4415a828f8dd39f67d265a9988f0da4a4b3c05
Axel <axeltib@kth.se>
2020-09-22 14:37:30 +0200

    added comments generally

--- exc1/package1/src/node_a.py
+++ exc1/package1/src/node_a.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-
-"""Acts as the publisher"""
-
 import rospy
 from std_msgs.msg import Int16
 
+"""Acts as the publisher only for the topic "tibbling"."""
+
 def publisher():
+    """Main function that runs the pubslisher. """
     pub = rospy.Publisher('tibbling', Int16, queue_size=1)
     rospy.init_node('publisher', anonymous = True)
-    rate = rospy.Rate(2) #Sets rate at 20 Hz
+    rate = rospy.Rate(20) #Sets rate at 20 Hz
     k = 0                 #Number to be sent
     n = 4                 #The increment for k with each loop
 
     while not rospy.is_shutdown():
-        k += n
-        rospy.loginfo(k)
+        k += n            #After 20 ms, send k + n to topic
+        rospy.loginfo(k)  #Used for troubleshooting
         pub.publish(k)
         rate.sleep()
 
--- exc1/package2/src/node_b.py
+++ exc1/package2/src/node_b.py
@@ -2,16 +2,22 @@
 import rospy
 from std_msgs.msg import Int16, Float32
 
+"""Subscriber and publisher. Subscribes to the topic "tibbling" and publishes on
+"/kthfs/result". """
+
 pub = rospy.Publisher("/kthfs/result", Float32, queue_size=1)
 q = 0.15
 
 def callback(data):
+    """Gets called when reading data from "tibbling", "bounces" modified data to
+    "kthfs/result". """
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     out_data = data.data/q   #Data to be sent to the topic "/kthfs/result"
     pub.publish(out_data)    #Publishinglogging for debugging
     rospy.loginfo(out_data)  #logging for debugging, optional
 
 def bounce():
+    """The main function that is running the subscriber. """
     rospy.init_node('buoncer', anonymous=True)
     rospy.Subscriber("tibbling", Int16, callback)
     rospy.spin()
